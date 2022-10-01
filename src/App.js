fc945c1bda92d6782d0263437137bb8fddd13e5d
san510 <47128979+san510@users.noreply.github.com>
2022-10-01 12:20:19 +0900

    Create Molecule

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { PrimaryButton } from "./components/atoms/PrimaryButton";
-import { SecondaryyButton } from "./components/atoms/SecondaryButton";
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryyButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <PrimaryButton>Test</PrimaryButton>
+      <PrimaryButton>テスト</PrimaryButton>
       <SecondaryyButton>検索</SecondaryyButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
