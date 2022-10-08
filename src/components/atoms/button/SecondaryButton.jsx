0213edddf76e874616c32d97620983270c46586f
san510 <47128979+san510@users.noreply.github.com>
2022-10-08 11:52:28 +0900

    Not Use Context

--- src/components/atoms/button/SecondaryButton.jsx
+++ src/components/atoms/button/SecondaryButton.jsx
@@ -2,9 +2,9 @@ import styled from "styled-components";
 import { BaseButton } from "./BaseButton";
 
 export const SecondaryyButton = (props) => {
-  const { children } = props;
+  const { children, onClick } = props;
 
-  return <SButton>{children}</SButton>;
+  return <SButton onClick={onClick}>{children}</SButton>;
 };
 
 const SButton = styled(BaseButton)`
--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,22 @@
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { SecondaryyButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
+  const history = useHistory();
+
+  const onClickAdmin = () =>
+    history.push({ pathname: "/users", state: { isAdmin: true } });
+  const onClickGeneral = () =>
+    history.push({ pathname: "/users", state: { isAdmin: false } });
+
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SecondaryyButton onClick={onClickAdmin}>管理者ユーザー</SecondaryyButton>
+      <br />
+      <br />
+      <SecondaryyButton onClick={onClickGeneral}>一般ユーザー</SecondaryyButton>
     </SContainer>
   );
 };
